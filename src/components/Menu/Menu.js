7c219cc7a8e11a759308813d8dd46745a25e9dd3
leonpibe <steven.deleonfeliz@gmail.com>
2022-07-14 00:16:13 +0800

    anality trie 1

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -7,7 +7,19 @@ export default function Menu(props) {
     const {menuColor} = props;
   return (
     <header className="menu" style={{backgroundColor: menuColor || "transparent"}}>
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-91093610-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments)}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-91093610-1');
+</script>
+
+
+
         <Container>
+          
             <ul>
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/skills">Skills</Link></li>
